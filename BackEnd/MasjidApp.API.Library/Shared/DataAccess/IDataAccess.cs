a6ddfa683b2571e17815dd27cac3f98844006b43
Zayd McArdle <zaydmcardle@outlook.com>
2024-08-29 18:23:03 +0100

    Refactor data access layer

--- BackEnd/MasjidApp.API.Library/Shared/DataAccess/IDataAccess.cs
+++ BackEnd/MasjidApp.API.Library/Shared/DataAccess/IDataAccess.cs
@@ -1,9 +1,40 @@
 namespace MasjidApp.API.Library.Shared.DataAccess;
 
-internal interface IDataAccess
+public interface IDataAccess : IDisposable
 {
     Task<int> ReadRecordCountFromDatabaseAsync<TParameters>(string storedProcedure, TParameters parameters);
-    Task<IEnumerable<TClass>> ReadRecordsFromDatabaseAsync<TClass, TParameters>(string storedProcedure, TParameters parameters);
-    Task<TClass> ReadFirstRecordFromDatabaseAsync<TClass, TParameters>(string storedProcedure, TParameters parameters);
-    Task WriteToDatabaseAsync<TParameters>(string storedProcedure, TParameters parameters);
+
+    /// <summary>
+    /// Returns a collection of data from an SQL query.
+    /// </summary>
+    /// <typeparam name="TClass">The class the data will be mapped tp</typeparam>
+    /// <param name="storedProcedure">The stored procedure you want your SQL provider to execute</param>
+    Task<IEnumerable<TClass>> ReadRecordsFromDatabaseAsync<TClass>(string storedProcedure);
+
+    /// <summary>
+    /// Returns a collection of data from an SQL query.
+    /// </summary>
+    /// <typeparam name="TClass">The class the data will be mapped tp</typeparam>
+    /// <typeparam name="TParameters">The parameter type; for example, dynamic, key value pair.</typeparam>
+    /// <param name="storedProcedure">The stored procedure you want your SQL provider to execute</param>
+    /// <param name="parameters">Parameters to be injected into the stored procedure</param>   
+    Task<IEnumerable<TClass>> ReadRecordsFromDatabaseWithParametersAsync<TClass, TParameters>(string storedProcedure, TParameters parameters);
+
+    /// <summary>
+    /// Returns a single record from a SQL Query
+    /// </summary>
+    /// <typeparam name="TClass">The class the data will be mapped tp</typeparam>
+    /// <param name="storedProcedure">The stored procedure you want your SQL provider to execute</param>
+    Task<TClass> ReadFirstRecordFromDatabaseAsync<TClass>(string storedProcedure);
+
+    /// <summary>
+    /// Returns a single record from a SQL Query
+    /// </summary>
+    /// <typeparam name="TClass">The class the data will be mapped tp</typeparam>
+    /// <typeparam name="TParameters">The parameter type; for example, dynamic, key value pair.</typeparam>
+    /// <param name="storedProcedure">The stored procedure you want your SQL provider to execute</param>
+    /// <param name="parameters">Parameters to be injected into the stored procedure</param>    
+    Task<TClass> ReadFirstRecordFromDatabaseWithParametersAsync<TClass, TParameters>(string storedProcedure, TParameters parameters);
+
+    Task WriteToDatabaseAsync<TParameters> (string storedProcedure, TParameters parameters);
 }
