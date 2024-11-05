796b7f9f92f26b9ef55d39611f55353bc0abc05b
zayd adam mcardle <zayd@localhost.localdomain>
2024-11-05 07:46:25 +0000

    Add dummy database initialisation scripts

---
+++ BackEnd/Database/scripts/init-database.sql
@@ -0,0 +1,20 @@
+-- Create database
+CREATE DATABASE IF NOT EXISTS masjidappdatabase;
+USE masjidappdatabase;
+
+-- Create tables
+
+
+-- Create stored procedures
+DELIMITER $$
+
+
+
+DELIMITER ;
+
+-- Create users
+
+
+--Adjust user permissions
+
+
