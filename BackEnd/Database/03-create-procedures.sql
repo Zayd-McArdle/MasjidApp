927472192cbebc3dac8481db208b95ff7c3ccb2a
Zayd Adam McArdle <zayd@Zayds-MacBook-Pro.local>
2025-06-27 11:48:07 +0100

    Refactor Database

--- BackEnd/Database/03-create-procedures.sql
+++ BackEnd/Database/03-create-procedures.sql
@@ -7,13 +7,13 @@ END //
 
 CREATE PROCEDURE IF NOT EXISTS get_user_credentials(IN p_username VARCHAR(200))
 BEGIN
-    SELECT username, password FROM user_details WHERE username = p_username;
+    SELECT username, password, role FROM user_details WHERE username = p_username;
 END //
 
-CREATE PROCEDURE IF NOT EXISTS register_user(IN p_first_name VARCHAR(50), IN p_last_name VARCHAR(50), IN p_role VARCHAR(50), IN p_email VARCHAR(50), IN p_username VARCHAR(200), IN p_password VARCHAR(200))
+CREATE PROCEDURE IF NOT EXISTS register_user(IN p_full_name VARCHAR(100), IN p_role VARCHAR(50), IN p_email VARCHAR(50), IN p_username VARCHAR(200), IN p_password VARCHAR(200))
 BEGIN
-    INSERT INTO user_details (first_name, last_name, role, email, username, password) 
-    VALUES (p_first_name, p_last_name, p_role, p_email, p_username, p_password);
+    INSERT INTO user_details (full_name, role, email, username, password) 
+    VALUES (p_full_name, p_role, p_email, p_username, p_password);
 END //
 
 CREATE PROCEDURE IF NOT EXISTS reset_user_password(IN p_username VARCHAR(200), IN p_password VARCHAR(200))
@@ -29,9 +29,60 @@ BEGIN
     SELECT file_data from prayer_times;
 END //
 
-CREATE PROCEDURE IF NOT EXISTS update_prayer_times_file(IN p_updated_prayer_times_file LONGBLOB)
+CREATE PROCEDURE IF NOT EXISTS upsert_prayer_times_file(IN p_prayer_times_file LONGBLOB)
 BEGIN
-    UPDATE prayer_times SET file_data = p_updated_prayer_times_file;
+    UPDATE prayer_times SET file_data = p_prayer_times_file;
+    IF ROW_COUNT() = 0 THEN 
+        INSERT INTO prayer_times (file_data) VALUES (p_prayer_times_file);
+    END IF;
 END //
+
+
+-- announcements stored procedures
+CREATE PROCEDURE IF NOT EXISTS get_announcements()
+BEGIN
+    SELECT a.id, a.title, a.description, a.last_updated, a.image, u.full_name
+    FROM announcements a
+    JOIN user_details u ON a.user_id = u.id;
+END //
+
+CREATE PROCEDURE IF NOT EXISTS post_announcement(IN p_title VARCHAR(50), IN p_description VARCHAR(50), IN p_image LONGBLOB, IN p_username VARCHAR(200))
+BEGIN
+    DECLARE v_user_id INT;
+    -- Retrieve user ID based on username
+    SELECT id INTO v_user_id 
+    FROM user_details 
+    WHERE username = p_username
+    LIMIT 1;
+
+    IF v_user_id IS NOT NULL THEN 
+        INSERT INTO announcements (title, description, image, user_id)
+        VALUES (p_title, p_description, p_image, v_user_id);
+        SELECT LAST_INSERT_ID();
+    ELSE
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'User not found';
+    END IF;
+END //
+
+CREATE PROCEDURE IF NOT EXISTS edit_announcement(IN p_id INT, IN p_username VARCHAR(200), IN p_title VARCHAR(50), IN p_description VARCHAR(50), IN p_image LONGBLOB)
+BEGIN 
+    DECLARE v_user_id INT;
+    -- Retrieve user ID based on username
+    SELECT id INTO v_user_id
+    FROM user_details
+    WHERE username = p_username
+    LIMIT 1;
+
+    IF v_user_id IS NOT NULL THEN
+        UPDATE announcements 
+        SET title = p_title, description = p_description, image = p_image 
+        WHERE id = p_id;
+    ELSE
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'User not found';
+    END IF;
+END //
+
 
 DELIMITER ;
