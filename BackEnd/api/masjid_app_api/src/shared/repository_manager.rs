3d9fdf384f79ff7dc037f7460fbbdd1f3e6ced5d
Zayd Adam McArdle <zayd@Zayds-MacBook-Pro.local>
2025-07-02 22:44:42 +0100

    Add logging to repository manager for easier diagnosis of technical issues

--- BackEnd/api/masjid_app_api/src/shared/repository_manager.rs
+++ BackEnd/api/masjid_app_api/src/shared/repository_manager.rs
@@ -1,5 +1,7 @@
 use sqlx::mysql::{MySqlPool, MySqlPoolOptions};
+use sqlx::{Error, MySql, Pool};
 use std::sync::Arc;
+
 #[derive(Hash, Eq, PartialEq)]
 pub enum RepositoryType {
     Authentication,
@@ -25,18 +27,34 @@ pub struct MySqlRepository {
 impl MySqlRepository {
     pub async fn new(repository_type: RepositoryType) -> Self {
         let connection_string_environment_variable = match repository_type {
-            RepositoryType::Authentication => "AUTHENTICATION_CONNECTION",
-            RepositoryType::PrayerTimes => "PRAYER_TIMES_CONNECTION",
-            RepositoryType::Announcement => "ANNOUNCEMENT_CONNECTION",
+            RepositoryType::Authentication => {
+                tracing::info!("establishing database connection for authenticating users");
+                "AUTHENTICATION_CONNECTION"
+            }
+            RepositoryType::PrayerTimes => {
+                tracing::info!("establishing database connection for retrieving prayer times");
+                "PRAYER_TIMES_CONNECTION"
+            }
+            RepositoryType::Announcement => {
+                tracing::info!("establishing database connection for retrieving announcements");
+                "ANNOUNCEMENT_CONNECTION"
+            }
         };
         let connection_string = std::env::var(connection_string_environment_variable).unwrap();
-        let db_connection = MySqlPoolOptions::new()
+        let db_connection_result = MySqlPoolOptions::new()
             .max_connections(10)
             .connect(&connection_string)
-            .await
-            .unwrap();
-        MySqlRepository {
-            db_connection: Arc::new(db_connection),
+            .await;
+        match db_connection_result {
+            Ok(db_connection) => {
+                tracing::info!("database connection successfully established");
+                Self {
+                    db_connection: Arc::new(db_connection),
+                }
+            }
+            Err(err) => {
+                panic!("Failed to connect to database: {err}");
+            }
         }
     }
 }
