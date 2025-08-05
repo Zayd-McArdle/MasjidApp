53d96d29cb270513f2c917bb1fdd31ea29033a58
Zayd McArdle <zaydmcardle@outlook.com>
2025-08-05 07:37:41 +0100

    Add validation to ContactDetails struct

--- BackEnd/api/masjid_app_api_library/src/shared/contact_details.rs
+++ BackEnd/api/masjid_app_api_library/src/shared/contact_details.rs
@@ -1,10 +1,13 @@
 use serde::{Deserialize, Serialize};
+use validator::Validate;
 
-#[derive(Deserialize, Serialize, Debug, Clone, PartialEq)]
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Validate)]
 pub struct ContactDetails {
+    #[validate(length(min = 3))]
     #[serde(rename(serialize = "fullName", deserialize = "fullName"))]
     pub full_name: String,
     #[serde(rename(serialize = "phoneNumber", deserialize = "phoneNumber"))]
     pub phone_number: String,
+    #[validate(email)]
     pub email: Option<String>,
 }
