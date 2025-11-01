582907014b4786d1fec01ca2569cb30981ef02a5
Zayd McArdle <zaydmcardle@outlook.com>
2025-11-01 11:34:42 +0000

    Make QuestionDTO, Answer & Reference structs convertable to/from JSON

--- BackEnd/api/masjid_app_api_library/src/features/ask_imam.rs
+++ BackEnd/api/masjid_app_api_library/src/features/ask_imam.rs
@@ -1,15 +1,20 @@
 use async_trait::async_trait;
-
+use serde::{Deserialize, Serialize};
+use validator::Validate;
+#[derive(Debug, Deserialize, Serialize, Validate)]
 pub struct Reference {
     source: String,
     url: String,
 }
+#[derive(Debug, Deserialize, Serialize, Validate)]
 pub struct Answer {
+    #[serde(rename = "imamName")]
     pub imam_name: String,
     pub text: String,
     pub reference: Option<Vec<Reference>>,
 }
 
+#[derive(Debug, Deserialize, Serialize, Validate)]
 pub struct QuestionDTO {
     pub id: i32,
     pub title: String,
