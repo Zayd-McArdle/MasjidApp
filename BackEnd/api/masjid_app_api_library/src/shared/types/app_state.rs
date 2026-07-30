463449071a778658c23228dd2d0ceaf4615b3d46
Zayd McArdle <zaydmcardle@outlook.com>
2026-07-30 19:55:19 +0100

    Backend: Remove AppState struct

--- BackEnd/api/masjid_app_api_library/src/shared/types/app_state.rs
+++ BackEnd/api/masjid_app_api_library/src/shared/types/app_state.rs
@@ -1,12 +1,3 @@
-use crate::shared::data_access::db_type::DbType;
-use std::collections::HashMap;
-
-#[derive(Default, Clone)]
-pub struct AppState<T> {
-    //This field allows you to write data to any database
-    pub repository_map: HashMap<DbType, T>,
-}
-
 #[derive(Clone)]
 pub struct ServiceAppState<T> {
     pub service: T,
